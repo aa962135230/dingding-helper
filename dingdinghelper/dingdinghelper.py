f4e19b73a3fda1ea82724220bbc71f03e3cda5c8
l2m2 <l2m2lq@gmail.com>
2018-12-28 07:47:01 +0800

    upload.

--- dingdinghelper/dingdinghelper.py
+++ dingdinghelper/dingdinghelper.py
@@ -5,7 +5,7 @@
 '''
 
 import os, json
-from urllib import request
+from urllib import request, parse
 
 class DingDingHelper:
   """钉钉助手
@@ -14,20 +14,30 @@ class DingDingHelper:
   def __init__(self, cfg):
     self._cfg = cfg
 
+  def get_access_token(self):
+    self._access_token = ""
+    params = parse.urlencode({'corpid': self._cfg['corpid'], 'corpsecret': self._cfg['corpsecret']})
+    url = 'https://oapi.dingtalk.com/gettoken?%s' % params
+    with request.urlopen(url) as f:
+      res = json.loads(f.read().decode('utf-8'))
+      if res.get("errmsg") == "ok":
+        self._access_token = res.get("access_token")
+    return self._access_token
+
   def send_msg(self, msg):
-    textmod = {
-        "msgtype": "text",
-        "text": { "content": msg },
-        "at": { "isAtAll": False }
+    data = {
+      "msgtype": "text",
+      "text": { "content": msg },
+      "at": { "isAtAll": False }
     }
-    textmod = json.dumps(textmod).encode(encoding='utf-8')
-    req = request.Request(url=self._cfg['msg_url'], data=textmod, headers={
-        "Content-Type": "application/json", "charset": "utf-8"
+    data = json.dumps(data).encode(encoding='utf-8')
+    req = request.Request(url=self._cfg['msg_url'], data=data, headers={
+      "Content-Type": "application/json", "charset": "utf-8"
     })
     res = request.urlopen(req)
     res = res.read()
     if not (json.loads(res).get('errmsg') == 'ok'):
-        self.send_msg(msg)
+      self.send_msg(msg)
 
 if __name__ == "__main__":
   if not os.path.exists('dingding.cfg'):
@@ -35,4 +45,5 @@ if __name__ == "__main__":
   with open('dingding.cfg', 'r') as f:
     cfg = json.load(f)
   ding = DingDingHelper(cfg)
-  ding.send_msg("这是一条测试消息")
+  # ding.send_msg("这是一条测试消息")
+  print(ding.get_access_token())
